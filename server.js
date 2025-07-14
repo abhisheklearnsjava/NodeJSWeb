719743ac124828e6fe342aa101f96731808eb122
abhisheklearnsjava <abhisheklearnsjava@gmail.com>
2025-07-14 17:48:01 -0400

    git ignore

--- server.js
+++ server.js
@@ -5,6 +5,12 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+// New route with a linting issue: unused variable
+app.get('/ping', (req, res) => {
+  const unused = 42; // ESLint will warn about 'unused' being defined but never used
+  res.send('pong');
+});
+
 app.listen(3000, () => {
   console.log(`Server running at http://localhost:3000`);
 });
